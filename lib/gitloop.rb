da1855a92018b7842bad09eac4ae1c056b2d8381
Alain Ravet <alain.ravet+github@gmail.com>
2012-02-21 12:10:59 +0100

    implement + test the `gitloop`command

--- lib/gitloop.rb
+++ lib/gitloop.rb
@@ -1,9 +1,8 @@
-require "gitloop/version"
-
 module Gitloop
 
   class GitError < Exception; end
 
 end
 
+require File.dirname(__FILE__) + "/gitloop/version"
 require File.dirname(__FILE__) + "/gitloop/looper"
